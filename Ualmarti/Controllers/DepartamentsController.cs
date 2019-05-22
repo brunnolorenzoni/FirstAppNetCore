775a0c41f324c2cfa43768aecd2431bfb2b9d90f
BRUNNO LORENZONI <801202062@fspoa.br>
2019-05-22 20:49:54 -0300

    first commit

---
+++ Ualmarti/Controllers/DepartamentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ualmarti.Models;
+
+namespace Ualmarti.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Departament> list = new List<Departament>();
+
+            list.Add(new Departament { Id = 1, Name = "brinquedos" });
+            list.Add(new Departament { Id = 2, Name = "alimentação" });
+            list.Add(new Departament { Id = 3, Name = "vestuario" });
+
+            return View(list);
+        }
+    }
+}
---
+++ Ualmarti/Models/Departament.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ualmarti.Models
+{
+    public class Departament
+    {
+
+        public int      Id      { get; set; }
+        public string   Name    { get; set; }
+
+    }
+}
